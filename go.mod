daaac7e0b9b0b2d02795fee544207029d80238a7
Ivan Malyi <i.malyi@fc-sistema.com>
2022-01-28 16:48:53 +0200

    1.0.0: add router

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v1.0.0
+	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.8.1
 )
--- internal/app/appserver/appserver.go
+++ internal/app/appserver/appserver.go
@@ -1,20 +1,23 @@
 package appserver
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
 )
 
 type AppServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config)*AppServer {
-
-
 	return &AppServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -23,9 +26,9 @@ func (server *AppServer) Start() error {
 	if err != nil {
 		return err
 	}
+	server.configureRouter()
 	server.logger.Info("server start listen")
-
-	return nil
+	return http.ListenAndServe(server.config.BindAddr, server.router)
 }
 
 func (server *AppServer)configureLogger() error {
@@ -36,4 +39,14 @@ func (server *AppServer)configureLogger() error {
 	server.logger.SetLevel(level)
 
 	return nil
+}
+
+func (server *AppServer) configureRouter()  {
+	server.router.HandleFunc("/hello", server.HandleHello())
+}
+
+func (server *AppServer) HandleHello() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		io.WriteString(writer, "hello world")
+	}
 }
